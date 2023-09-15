b04c4f3009c61b4b38b110a4e7a27e1e3e3da18b
muggerseo <mugger.seo@gmail.com>
2023-09-15 23:17:12 +0300

    numpad removed and made some minor changes

--- calculator.py
+++ calculator.py
@@ -38,78 +38,100 @@ def operator(a, b, operator):
     elif operator == '*':
         result = calc.multiply(a, b)
     elif operator == '/':
-        if b == 0:
+        if operator == '/' and b == 0:
             messagebox.showerror("Error!!!", "Can't be divided by 0!")
+            input_2.focus_set()
         else:
             result = calc.divide(a, b)
+    elif operator not in ('+','-','/','*'):
+            messagebox.showerror("Error!!!", "only + - / * operators acceptable!")
+            input_3.focus_set()
     else:
         result = "Invalid operator!"
 
     return result
-       
+
+def reset_fields():
+    input_1.delete(0, tk.END)
+    input_2.delete(0, tk.END)
+    input_3.delete(0, tk.END)
 #==================program interface start========================        
 window = tk.Tk()
 window.title("Calculator")
 window.resizable(True,True)
-window.geometry('300x400')
+window.geometry('250x300')
 
 #----create a frame for the input fields----
 input_frame = tk.Frame(window)
 input_frame.grid(pady=20)
 
-label_1 = tk.Label(input_frame, text='First number: ')
+label_1 = tk.Label(input_frame, text='First number: ', font=("Helvetica", 10, 'bold'))
 label_1.grid(row=0, column=0)
 input_1 = tk.Entry(input_frame, bg='white')
 input_1.grid(row=0, column=1)
+input_1.focus_set()
+input_1.bind('<Return>', lambda event: input_2.focus_set())
 
-label_2 = tk.Label(input_frame, text='Second number: ')
+label_2 = tk.Label(input_frame, text='Second number: ', font=("Helvetica", 10, 'bold'))
 label_2.grid(row=1, column=0)
 input_2 = tk.Entry(input_frame, bg='white')
-input_2.grid(row=1, column=1, padx=10)
+input_2.grid(row=1, column=1)
+input_2.bind('<Return>', lambda event: input_3.focus_set())
 
-label_3 = tk.Label(input_frame, text='Operation: ')
+label_3 = tk.Label(input_frame, text='Operation: ', font=("Helvetica", 10, 'bold'))
 label_3.grid(row=2, column=0)
 input_3 = tk.Entry(input_frame, bg='white')
 input_3.grid(row=2, column=1)
+input_3.bind('<Return>', lambda event: calc_button.focus_set())
 
-calc_button = tk.Button(input_frame, text="Calculate", command=calculate)
-calc_button.grid(row=4, columnspan=1, pady=5)
+calc_button = tk.Button(input_frame, text="Calculate", width=10, height=2, command=calculate)
+calc_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+calc_button.bind('<Return>', lambda event=None:calculate())
 
 result_label = tk.Label(input_frame, text='Result: ')
-result_label.grid(row=5, columnspan=1)
+result_label.grid(row=4, column=0)
+
+reset_button = tk.Button(input_frame, text="Reset", bg='grey', fg='red', width=10, height=2, command=reset_fields)
+reset_button.grid(row=5, column=0, columnspan=2, pady=30)
 
 # Create a frame for the numeric keypad
-keypad_frame = tk.Frame(window)
-keypad_frame.grid(row=6, column=1, pady=20)
+# keypad_frame = tk.Frame(window)
+# keypad_frame.grid()
+
+# enter_button = tk.Button(input_frame, text='Next number')
+# enter_button.grid(row=7, column=0, columnspan=3, pady=20)
 
 #======input=======
-
 input_1.focus_set()
-
 #=======NUM keys pannel=======
 
-class CalculatorApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Calculator")
+# class CalculatorApp:
+#     def __init__(self, root):
+#         self.root = root
+#         self.root.title("Calculator")
 
-        self.entry = tk.Entry(root)
-        self.entry.grid(row = 0, column = 0, columnspan = 3)
+#         self.entry = tk.Entry(root)
+#         self.entry.grid(row=6, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
 
-        for i in  range(1, 10):
-            row_num = (i - 1) // 3 + 1
-            col_num = (i - 1) % 3
-            if i == 9:
-                col_num = 2
-            button = tk.Button(root, text=str(i), command=lambda i=i: self.on_digital_click(i))
-            button.grid(row=row_num, column=col_num)
+#         self.current_number = "" # Initialize a variable to store the current number
 
-    def on_digital_click(self, digit):
-        current_text = self.entry.get()
-        self.entry.delete(0, tk.END)
-        self.entry.insert(0, current_text + str(digit))
+#         for i in  range(1, 10):
+#             row_num = (i - 1) // 3 + 1
+#             col_num = (i - 1) % 3
+          
+#             button = tk.Button(root, text=str(i), command=lambda digit=i: self.on_digital_click(digit))
+#             button.grid(row=row_num, column=col_num, sticky="nsew")
 
-app = CalculatorApp(window)
+#     def on_digital_click(self, digit):
+#         current_text = self.entry.get()
+#         self.entry.delete(0, tk.END)
+#         self.entry.insert(0, current_text + str(digit))
+
+#     def move_to_first_number(self):
+#         self.current_number = "" # clear the current number
+#         self.entry.delete(0, tk.END) # clear the entry field
+
+# app = CalculatorApp(window)
 
 if __name__ == '__main__':
     window.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-calc_button = tk.Button(input_frame, text="Calculate", command=calculate)
-# calc_button.grid(row=3, columnspan=2)
+
